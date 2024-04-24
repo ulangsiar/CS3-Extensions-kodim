547edebf40366dc6dc299f87cd74e1780a981388
ulangsiar <135447152+ulangsiar@users.noreply.github.com>
2024-04-24 20:53:57 +0800

    Update TestPlugin
    Create new code

---
+++ Test/src/main/kotlin/com/kodim/TestPlugin.kt
@@ -0,0 +1,12 @@
+package com.kodim
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class TestPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(TestProvider())
+    }
+}
