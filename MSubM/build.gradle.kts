5c37822a990377a892716624f3226f5c5413b62f
ulangsiar <135447152+ulangsiar@users.noreply.github.com>
2024-04-25 20:32:16 +0800

    New Provider
    Create code

---
+++ MSubM/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+    language = "en"
+
+    description = "Contains favourites movies and series"
+
+    authors = listOf("kodim")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://ww13.moviesubmalay.online/&sz=%size%"
+}
