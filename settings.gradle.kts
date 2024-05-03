f88ae93d31964b402efbb9fdadb0916576174ab4
ulangsiar <135447152+ulangsiar@users.noreply.github.com>
2024-05-03 22:10:04 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 
-val disabled = listOf<String>("Bioskop","PMovie")
+val disabled = listOf<String>("Bioskop")
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
@@ -16,4 +16,4 @@ fun File.eachDir(block: (File) -> Unit) {
 
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-include("LK21")
+//include("LK21")
