068d27e7d744af9cd9809c3505ce605057b47ee8
ulangsiar <135447152+ulangsiar@users.noreply.github.com>
2024-05-14 21:16:31 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.2.2")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.codingfeline.buildkonfig.buildkonfig-gradle-plugin:0.15.1")
     }
 }
 
@@ -85,6 +86,6 @@ subprojects {
     }
 }
 
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+task.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
