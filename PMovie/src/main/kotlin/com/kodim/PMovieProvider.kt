5179796bcd4887d3707821b265c568b2da40cb93
ulangsiar <135447152+ulangsiar@users.noreply.github.com>
2024-05-15 19:36:54 +0800

    Update PMovieProvider.kt

--- PMovie/src/main/kotlin/com/kodim/PMovieProvider.kt
+++ PMovie/src/main/kotlin/com/kodim/PMovieProvider.kt
@@ -54,8 +54,7 @@ class PMovieProvider : MainAPI() {
                 addSub(episode)
             }
         } else {
-            val quality = this.selectFirst("span.mli-quality").text().trim().replace("-", "")
-            Log.d("DEBUG", quality)            
+            val quality = this.selectFirst("span.mli-quality").text().trim().replace("-", "").replace("Rip", "")
             newMovieSearchResponse(title, href, TvType.Movie) {
                 this.posterUrl = posterUrl
                 addQuality(quality)
@@ -77,23 +76,16 @@ class PMovieProvider : MainAPI() {
         val title = document.selectFirst("div.mvic-desc h3")?.text()?.trim() ?: return null
         val poster = fixUrlNull(document.selectFirst("div.thumb.mvic-thumb img")?.attr("src"))
         val tags = document.select("div.mvici-left p:nth-child(1) a").map { it.text() }
-        val year = document.select("div.mvici-right p:nth-child(3) a").text().trim()
-            .toIntOrNull()
-
-        val tvType = if (document.selectFirst("div.les-content")
-                ?.select("a")?.size!! > 1 || document.selectFirst("ul.idTabs li strong")?.text()
-                ?.contains(Regex("(?i)(EP\\s?[0-9]+)|(episode\\s?[0-9]+)")) == true
-        ) TvType.TvSeries else TvType.Movie
-
+        val tvType = if (url.contains("/series/")) TvType.TvSeries else TvType.Movie
         val description = document.selectFirst("p.f-desc")?.text()?.trim()
         val trailer = fixUrlNull(document.select("iframe#iframe-trailer").attr("src"))
-        val rating = document.select("div.mvici-right > div.imdb_r span").text().toRatingInt()
-        val actors = document.select("div.mvici-left p:nth-child(3) a").map { it.text() }
-        val recommendations = document.select("div.ml-item").mapNotNull {
-            it.toSearchResult()
-        }
+
+        val recommendations = document.select("div.ml-item").mapNotNull { it.toSearchResult() }
 
         return if (tvType == TvType.TvSeries) {
+            val year = document.select("div.mvici-right p:nth-child(2) a").text().trim().toIntOrNull()
+            val actors = document.select("div.mvici-left p:nth-child(2) a").map { it.text() }
+            val rating = document.select("div.mv-rating div[id=movie-mark]").text().toRatingInt()
             val episodes = if (document.selectFirst("div.les-title strong")?.text().toString()
                     .contains(Regex("(?i)EP\\s?[0-9]+|Episode\\s?[0-9]+"))
             ) {
@@ -126,7 +118,9 @@ class PMovieProvider : MainAPI() {
             }
 
         } else {
-
+            val year = document.select("div.mvici-right p:nth-child(3) a").text().trim().toIntOrNull()
+            val rating = document.select("div.imdb_r span").text().toRatingInt()
+            val actors = document.select("div.mvici-left p:nth-child(3) a").map { it.text() }
             newMovieLoadResponse(title, url, TvType.Movie, url) {
                 this.posterUrl = poster
                 this.year = year
@@ -169,7 +163,6 @@ class PMovieProvider : MainAPI() {
                     }
                 }
         } else {
-
             loadExtractor(data, "$mainUrl/", subtitleCallback, callback)
         }
         return true
